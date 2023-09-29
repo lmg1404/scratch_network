3993da3e47025f302c407631bf97618e79385c5c
lmg1404 <luismg0203@gmail.com>
2023-09-29 18:51:32 -0400

    Added str dunder, forward prop implement

--- Layer.py
+++ Layer.py
@@ -1,7 +1,8 @@
 import numpy as np
 
+np.random.seed(0)
 class Layer:
-    def __init__(self, input_shape, output_shape, initializiation="random"):
+    def __init__(self, output_shape: int, input_shape: int, initializiation="random"):
         if initializiation.lower() == "random":
             self.weights = np.random.randn(output_shape, input_shape)
             self.bias = np.random.randn(output_shape, 1)
@@ -18,6 +19,16 @@ class Layer:
             
         else:
             raise TypeError("Initialization not random, zero, or he-et-al")
+        
+    def forward(self, input):
+        # ensure the shapes are correct
+        try:
+            assert self.weights.shape[1] == input.shape[0]
+        except:
+            raise ValueError(f"Shapes do not match {self.weights.shape} != {input.shape}")
+            
+        return self.weights@input + self.bias
+        
     
     def back_prop(self):
         # TODO: look for resources to create back propagration
@@ -32,3 +43,18 @@ class Layer:
             return self.bias
         else:
             raise TypeError("Pick a parameter to view")
+        
+    def __str__(self):
+        return f"""Weights shape: {self.weights.shape}
+Weights: {self.weights}
+Bias shape: {self.bias.shape}
+Bias: {self.bias}"""
+        
+if __name__ == "__main__":
+    layer = Layer(3, 4)
+    print(layer)
+    
+    a = np.random.randn(4,3)
+    print(a.shape)
+    output = layer.forward(a)
+    print(output)
