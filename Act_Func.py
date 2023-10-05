8160a1b03d2c1ccc15036972f8933228edf0ef94
lmg1404 <luismg0203@gmail.com>
2023-10-05 16:56:35 -0400

    Set up activation functions to be used

---
+++ Act_Func.py
@@ -0,0 +1,23 @@
+import numpy as np
+from Activation import Activation
+
+class Sigmoid(Activation):
+    def __init__(self):
+        # super().__init__(#sigmoid, #derivative of sigmoid)
+        pass            
+    # I don't think I have to define the backwards and forwards again?
+    
+class ReLU(Activation):
+    def __init__(self):
+        # super().__init___(#relu, #derivative of relu)
+        pass
+    
+class Tanh(Activation):
+    def __init__(self):
+        # super().__init__(blah)
+        pass
+
+class Softmax(Activation):
+    def __init__(self):
+        # super().__init__(blah)
+        pass
