d55cd21cb21627c7d5fe4e1af2934f4811d18135
lmg1404 <luismg0203@gmail.com>
2023-10-05 17:09:28 -0400

    Set up network class for training

--- Network.py
+++ Network.py
@@ -2,14 +2,28 @@ import numpy as np
 from Dense import Dense
 
 class Network:
-    def __init__(self):
-        # TODO layers, descent method?
-        pass
+    def __init__(self, layers=None, loss_func):
+        self.layers = layers # list or nothing there
+        self.loss_func = loss_func # class?
     
-    def train(self):
-        # TODO epochs, training, validation, etc:
-        pass
+    def train(self, epochs : int, train_data, val_data, verbose=True):
+        for i in range(epochs):
+            for x, y in train_data:
+                # TODO: train forward, maybe add minibatches to make faster?
+                pass
+            
+            for x, y in val_data:
+                # TODO: validation forward and loss
+                pass
+            
+            if verbose:
+                print(f"Epoch: {i+1} / Train loss: / Validation Loss: ")
     
     def predict(self):
         # TODO: just forward prop through the prediction examples
+        pass
+    
+    def history(self):
+        # TODO: get training history like in TF and PyTorch
+        # think this is a good idea
         pass
