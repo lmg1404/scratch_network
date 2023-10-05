c6bce0d01c9f54f555d9941bbc78a3c266f6069e
lmg1404 <luismg0203@gmail.com>
2023-10-05 17:37:42 -0400

    Clean up comments

--- Activation.py
+++ Activation.py
@@ -2,12 +2,16 @@ from Layer import Layer
 
 class Activation(Layer):
     def __init__(self, forward_func, backwards_func):
+        # these are gonna be functions, makes it easier
         self.forward_func = forward_func
         self.backwards_func = backwards_func
         
     def forward(self, input):
+        # input is saved for back prop
+        # see my onenote on iPad
         self.input = input
         return self.forward_func(self.input)
     
     def backward(self, grad):
-        return grad * self.backward_func(self.input) # this should be a hadamard product, scalar by vector/element wise for vectors
+        # this should be a hadamard product, scalar by vector/element wise for vectors
+        return grad * self.backward_func(self.input) 
