cda0717546d43f508ee4d8d5e94169b32f3cf5cc
lmg1404 <luismg0203@gmail.com>
2023-10-05 17:35:09 -0400

    Implemented add method

--- Network.py
+++ Network.py
@@ -1,10 +1,21 @@
 import numpy as np
 from Dense import Dense
+from History import History
 
 class Network:
-    def __init__(self, layers=None, loss_func):
+    def __init__(self, loss_func, layers=None):
         self.layers = layers # list or nothing there
         self.loss_func = loss_func # class?
+        self.history = History()
+    
+    def add(self, layers : list): # just adding a list makes everything much easier
+        if self.layers:
+            if type(self.layers) == list: # layers is a list no problem
+                self.layers += layers # list plus list is still 1D list
+            else: # layers is a dense object or some other object then we do something else
+                self.layers = [self.layers] + layers # this should make it a list
+        else:
+            self.layers = layers
     
     def train(self, epochs : int, train_data, val_data, verbose=True):
         for i in range(epochs):
@@ -25,5 +36,11 @@ class Network:
     
     def history(self):
         # TODO: get training history like in TF and PyTorch
-        # think this is a good idea
-        pass
+        return self.history
+    
+    
+def to_list(arg):
+    our_list = ['bug', 'wheat']
+    print([arg] + our_list)
+    
+to_list(6)
